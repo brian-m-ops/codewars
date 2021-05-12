ffdde88c0b66b0d747456d09d3c042cebde624b5
brian-m-ops <bryzey@gmail.com>
2021-05-12 19:21:51 +1000

    Completed Kata

--- 7kyu/growth_of_a_population.py
+++ 7kyu/growth_of_a_population.py
@@ -1,6 +1,9 @@
 def nb_year(population, percent, aug, target):
     year = 0
     while population < target:
-        population += population * percent / 100. + aug
+        population += population * percent // 100. + aug
         year += 1
     return year
+
+
+print(nb_year(1000, 2, 50, 1214))
