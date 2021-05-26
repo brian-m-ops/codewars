5dbaf3d7e9c85b826533e7b855dbe40f6a1f48a3
brian-m-ops <bryzey@gmail.com>
2021-05-27 07:54:50 +1000

    Completed Kata

---
+++ 7kyu/find_the_next_perfect_square.py
@@ -0,0 +1,13 @@
+import math
+
+
+def find_next_square(sq):
+    if (math.sqrt(sq)).is_integer():
+        return int(math.sqrt(sq) + 1) ** 2
+    else:
+        return -1
+
+
+sq = 25
+
+print(find_next_square(sq))
