7a65a807dee4c55f485d9def9edceab2f937f607
brian-m-ops <bryzey@gmail.com>
2021-07-10 14:18:21 +1000

    Completed Kata

---
+++ 7kyu/dont_give_me_five.py
@@ -0,0 +1,9 @@
+def dont_give_me_five(start, end):
+    count = 0
+    for i in range(start, end + 1):
+        if str(5) not in str(i):
+            count += 1
+    return count
+
+
+print(dont_give_me_five(4, 17))
